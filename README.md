8feb48af7398e49b40fd6c17cce3470560ee925c
Abhishek Aggarwal <abhi@abhitopia.com>
2015-08-23 15:44:01 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# MovieLensAnalyze
+# MovieLensAnalysis
+Dataset: http://grouplens.org/datasets/movielens/
+
+#Problem Statement
+./analyze_movies.py (Gender|Age) <number>
+
+The first argument is the grouping. For example, if gender is selected, then two lists will be output: top movies as rated by men, and top movies as rated by women. If agegroup is selected, 7 lists should be output, as specified in the dataset README. The <number> is how many to output. If there are fewer than <number> movies, output them all.
+An example invocation would, therefore be "./analyze_movies.py age 20"
+
+#Defining the concept of **Top** movies
+The problem statement above is very vague in terms of defining what **top** movies mean. One simple way would be to order by 
+average rating of the movie. However, the problem with this is that our belief with movies that are rated by very few number of
+users can skew the ordering. Consider for example a movie X that has a single rating of "5". Clearly, the average rating of this
+ movie is going to be "5" as well. However, since only one user rated this movie, our belief in this should be very low. 
+ To account for this, number of users should be reflected in the score. Naive way would be to simple multiply average rating 
+ with the number of users ( to got total rating). Again, this has the problem that movies that are rated by a lot of people
+ will get higher rating than rating watched by fewer number of people even if the average rating of former is less than the latter.
+ To tackle this, I multiply with the logarithm of number of users instead. The idea is that out belief should linearly go up with the number 
+ of users initial, however, after a limit it should plateau out.
+ 
+
+#Note
+
+*   Although **sqllite3** is part of standard library, however, I think using it will defeat the purpose so I have used my own class. It will be pretty simple to construct though
+
+```sql
+    CREATE TABLE movielens 
+    AS
+    SELECT 
+         r.*,
+         u.*,
+         m.*
+    FROM 
+        ratings AS r
+    JOIN
+        movies AS m
+    ON
+        r.MovieID = m.MovieID
+    JOIN
+        users AS u
+    ON 
+        u.UserID = r.UserID;
+        
+        
+    SELECT 
+        Gender,
+        Title,
+        SumRating/NumUsers AS AvgRating,
+        NumUsers,
+        (SumRating/NumUsers) * LOG(NumUsers) AS Score
+    From
+    (
+        SELECT
+            Gender,
+            Title,
+            sum(Rating) AS SumRating,
+            count(1) AS NumUsers
+        FROM movielens
+        GROUP BY
+            Gender
+    ) AS x
+    ORDER BY
+        Score DESC
+    LIMIT 20;
+```
+
+*   No Machine Learning is used.
---
+++ analyze_movies.py
@@ -0,0 +1,107 @@
+import sys
+from operator import itemgetter
+import math
+
+MOVIES_FILE = './Data/ml-1m/movies.dat'
+RATINGS_FILE = './Data/ml-1m/ratings.dat'
+USERS_FILE = './Data/ml-1m/users.dat'
+
+
+class Table:
+    def __init__(self, columns, file_path=None, data=None):
+        self.columns = columns
+        self.file_path = file_path
+        self.data = data
+        if data is None:
+            self.load_from_file()
+
+    def load_from_file(self):
+        # reads the file into the memory
+        with open(self.file_path, 'r') as csv_file:
+            self.data = [list(row.split('::')) for row in csv_file.readlines()]
+
+    def __hash_on(self, column):
+        key_index = self.columns.index(column)
+        result = {}
+        for row in self.data:
+            hash_val = row[key_index]
+            if hash_val not in result:
+                result[hash_val] = []
+            result[hash_val].append(row)
+
+        return result
+
+    def join(self, other_table, column):
+        # in place joins the other_table on column
+        key_index = self.columns.index(column)
+        other_table_dict = other_table.__hash_on(column)
+        self.data = [row + other_table_dict[row[key_index]][0] for row in self.data]
+        self.columns = self.columns + other_table.columns
+
+    def group_by(self, column):
+        # groups by "column" and returns dictionary of Tables for each group by value
+        group_by_index = self.columns.index(column)
+        result = {}
+        for row in self.data:
+            group_by_val = row[group_by_index]
+            if group_by_val not in result:
+                result[row[group_by_index]] = Table(columns=self.columns, data=[])
+            result[group_by_val].data.append(row)
+        return result
+
+    def order_by(self, columns, desc=False):
+        # in place order by on columns in the order specified
+        sort_by_indices = [self.columns.index(column) for column in columns]
+        self.data = sorted(self.data, key=itemgetter(*sort_by_indices), reverse=desc)
+
+    def pretty_print(self, start, end):
+        print "\n" + '-' * 136 + '\n' + "{0:^10}|{1:^10}|{2:^80}|{3:^10}|{4:^10}|{5:^10}|".format(
+            'Index', *self.columns) + '\n' + '-' * 136
+        for idx in range(start, min(end, len(self))):
+            print "{0:^10}|{1:^10}|{2:^80}|{3:^10.4f}|{4:^10}|{5:^10.4f}|".format(idx+1, *self.data[idx])
+        print '-' * 136
+
+    def __len__(self):
+        # return number of rows in table
+        return len(self.data)
+
+    def __getitem__(self, key):
+        # return either complete column or slice of rows.
+        if isinstance(key, str):
+            idx = self.columns.index(key)
+            return [row[idx] for row in self.data]
+        elif isinstance(key, slice):
+            return self.data[key]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "Usage:  ./analyze_movies.py (Gender|Age) <number>"
+        sys.exit(1)
+    else:
+        # Reading command line arguments
+        group_by, num_rows = sys.argv[1], int(sys.argv[2])
+
+    # Joining the three files (movies, ratings and users) together
+    movies = Table(columns=['MovieID', 'Title', 'Genres'], file_path=MOVIES_FILE)
+    users = Table(columns=['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code'], file_path=USERS_FILE)
+    ratings = Table(columns=['UserID', 'MovieID', 'Rating', 'Timestamp'], file_path=RATINGS_FILE)
+    ratings.join(movies, 'MovieID')
+    ratings.join(users, 'UserID')
+
+    # Actual group by here
+    result = ratings.group_by(group_by)
+    for group in result.keys():
+        # Withing each group, all movies are further grouped by title
+        movies_agg = result[group].group_by('Title')
+        movie_avg_rating = Table(columns=['Group', 'Title', 'AvgRating', 'NumUsers', 'Score'], data=[])
+        for title, movie_table in movies_agg.items():
+            # calculating score for each movie within a group
+            num_users = len(movie_table)
+            avg_rating = sum(int(val) for val in movie_table['Rating']) / float(num_users)
+            score = avg_rating*math.log(num_users)
+            movie_avg_rating.data.append([group, title, avg_rating, num_users, score])
+
+        # Order by 'Score'
+        movie_avg_rating.order_by(['Score', 'AvgRating', 'NumUsers'], desc=True)
+        movie_avg_rating.pretty_print(0, num_rows)
